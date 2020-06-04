162133f6eef7a1fc03876f0dcc022c0c64c69969
Wojciech Kuzmik <wojciech.kuzmik@o2.pl>
2020-06-04 17:40:32 +0200

    dzialaja maski z zamiana koloru

--- GUIMyFrame1.cpp
+++ GUIMyFrame1.cpp
@@ -6,6 +6,8 @@ GUIMyFrame1::GUIMyFrame1(wxWindow* parent)
 	MyFrame1(parent)
 {
 	Bind(wxEVT_SIZE, &GUIMyFrame1::Form_Resize, this);
+	color_choice = 'R';
+	mask_choice = 0;
 }
 
 void GUIMyFrame1::load_imageOnButtonClick(wxCommandEvent& event)
@@ -14,13 +16,14 @@ void GUIMyFrame1::load_imageOnButtonClick(wxCommandEvent& event)
 	wxFileDialog WxOpenFileDialog(this, wxT("Choose a file"), wxT(""), wxT(""), wxT("Base image file (*.bmp)|*.bmp"), wxFD_OPEN | wxFD_FILE_MUST_EXIST);
 	if (WxOpenFileDialog.ShowModal() == wxID_OK)
 	{
-		if (!image.LoadFile((const_cast<char*>((const char*)WxOpenFileDialog.GetPath().mb_str())), wxBITMAP_TYPE_ANY))
+		if (!bitmap_image.LoadFile((const_cast<char*>((const char*)WxOpenFileDialog.GetPath().mb_str())), wxBITMAP_TYPE_ANY))
 		{
 			wxMessageBox(_("Nie uda\u0142o si\u0119 za³adowaæ obrazka"));
 		}
 	}
-	if(image.IsOk())
-		Repaint();
+	img_org = bitmap_image.ConvertToImage();
+	img_cpy = img_org.Copy();
+	Repaint();
 }
 
 void GUIMyFrame1::load_maskOnButtonClick(wxCommandEvent& event)
@@ -29,30 +32,28 @@ void GUIMyFrame1::load_maskOnButtonClick(wxCommandEvent& event)
 	wxFileDialog WxOpenFileDialog(this, wxT("Choose a file"), wxT(""), wxT(""), wxT("Mask file (*.bmp)|*.bmp"), wxFD_OPEN | wxFD_FILE_MUST_EXIST);
 	if (WxOpenFileDialog.ShowModal() == wxID_OK)
 	{
-		if (!mask_image.LoadFile((const_cast<char*>((const char*)WxOpenFileDialog.GetPath().mb_str())), wxBITMAP_TYPE_ANY))
+		if (!bitmap_mask.LoadFile((const_cast<char*>((const char*)WxOpenFileDialog.GetPath().mb_str())), wxBITMAP_TYPE_ANY))
 		{
 			wxMessageBox(_("Nie uda\u0142o si\u0119 za³adowaæ maski"));
 		}
 		else
 		{
 			//jak ustawi sie ta maske to w sumie wizualnie nic nie daje wiec nie wiem xd
-			mask =new wxMask(mask_image, base_color);
+			mask = bitmap_mask.GetMask();
 		}
 	}
-	if (image.IsOk())
-	{
-		image.SetMask(mask);
-		Repaint();
-	}
+	img_mask = bitmap_mask.ConvertToImage();
+	set_mask();
 }
 
 void GUIMyFrame1::save_imageOnButtonClick(wxCommandEvent& event)
 //zapisuje do pliku to co jest w new_image ( na razie nic )
 {
+	wxBitmap save(img_cpy);
 	wxFileDialog WxSaveFileDialog(this, wxT("Choose a file"), wxT(""), wxT(""), wxT("config files (*.bmp)|*.bmp"), wxFD_SAVE);
-	if (WxSaveFileDialog.ShowModal() == wxID_OK && new_image.IsOk()) 
+	if (WxSaveFileDialog.ShowModal() == wxID_OK) 
 	{
-		if (!new_image.SaveFile((const_cast<char*>((const char*)WxSaveFileDialog.GetPath().mb_str())), wxBITMAP_TYPE_BMP))
+		if (!save.SaveFile((const_cast<char*>((const char*)WxSaveFileDialog.GetPath().mb_str())), wxBITMAP_TYPE_BMP))
 		{
 			wxLogError(_("Nie uda\u0142o si\u0119 zapisaæ obrazka"));
 		}
@@ -63,38 +64,63 @@ void GUIMyFrame1::mask_optionsOnRadioBox(wxCommandEvent& event)
 //ustala zmienna pomocnicza mask_choice na odpowiedni int
 {
 	mask_choice = mask_options->GetSelection();
-	
-	if (image.IsOk())
-		Repaint();
+	set_mask();
 }
 
 void GUIMyFrame1::color_optionsOnRadioBox(wxCommandEvent& event)
 //ustala zmienna pomocnicza "base_color" na odpowiedni kolorek
 {
 	if (color_options->GetSelection() == 0)
-		base_color = wxColor(255, 0, 0);
+		color_choice = 'R';
 	else if (color_options->GetSelection() == 1)
-		base_color = wxColor(0, 255, 0);
+		color_choice = 'G';
 	else
-		base_color = wxColor(0, 0, 255); 
-
+		color_choice = 'B';
+	set_mask();
 	//trzeba zrobiæ ¿eby aktualizowalo kolor w masce po zmianie
+}
+
+void GUIMyFrame1::set_mask() 
+//glowna funkcja to 
+{
+	img_cpy = img_org.Copy();
+	int w = img_mask.GetWidth();
+	int h = img_mask.GetHeight();
+	int size = w * h * 3;
+	unsigned char *data = img_cpy.GetData();
+	unsigned char *mask_data = img_mask.GetData();
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			if (color_choice == 'R') {
+				data[3 * w * i + 3 * j + 1] = mask_data[3 * w * i + 3 * j + 1];
+				data[3 * w * i + 3 * j + 2] = mask_data[3 * w * i + 3 * j + 2];
+			}
+			else if (color_choice == 'G') {
+				data[3 * w * i + 3 * j] = mask_data[3 * w * i + 3 * j];
+				data[3 * w * i + 3 * j + 2] = mask_data[3 * w * i + 3 * j + 2];
+			}
+			else {
+				data[3 * w * i + 3 * j] = mask_data[3 * w * i + 3 * j];
+				data[3 * w * i + 3 * j + 1] = mask_data[3 * w * i + 3 * j + 1];
+			}
+		}
+	}
+
+	Repaint();
 }
 
 void GUIMyFrame1::Repaint()
 //rysuje bitmape obrazka i na razie nie robi nic z maska
-{
+{	
+	wxBitmap bitmap(img_cpy);
 	wxClientDC dc(m_panel1);
-	int	height = image.GetHeight();
-	int	width = image.GetWidth();
-	m_panel1->SetSize(image.GetSize());
-	dc.DrawBitmap(image, 0, 0, true); 
-
+	m_panel1->SetSize(img_cpy.GetSize());
+	dc.DrawBitmap(bitmap, 0, 0, true); 
 }
 
 void GUIMyFrame1::Form_Resize(wxSizeEvent & e)
 //jesli obrazek jest wczytany i zmieniamy rozmiar okna to robi repaint
 {
-	if (image.IsOk())
+	if(img_cpy.IsOk())
 		Repaint();
 }
--- GUIMyFrame1.h
+++ GUIMyFrame1.h
@@ -22,17 +22,21 @@ protected:
 	void color_optionsOnRadioBox(wxCommandEvent& event);
 	void Form_Resize(wxSizeEvent & e);
 	void Repaint();
+	void set_mask();
 public:
 	/** Constructor */
 	GUIMyFrame1(wxWindow* parent);
 	//// end generated class members
 
-	wxBitmap image;	//zmienna przechowujaca obrazek tla
-	wxBitmap mask_image; // zmienna przechowujaca obrazek maski
+	wxBitmap bitmap_image;	//zmienna przechowujaca obrazek tla
+	wxBitmap bitmap_mask; // zmienna przechowujaca obrazek maski
 	wxMask* mask = nullptr; //zmienna przechowujaca maske 
-	wxBitmap new_image; //zmienna w ktorej bedziemy przechowywac ta kombinacje obrazka i maski do zapisu
+	wxImage img_org; //zapisujemy tu wczytana bitmape
+	wxImage img_cpy; //dokonujemy zmian na tej kopii obrazka
+	wxImage img_mask; //zapisujemy tu wczytana maske
+	//wxBitmap new_image; //zmienna w ktorej bedziemy przechowywac ta kombinacje obrazka i maski do zapisu
 	int mask_choice; // pomocniczy int do wyboru sposobu kombinacji obrazka z maska
-	wxColor base_color; // pomocnicza zmienna wybranego koloru w masce
+	char color_choice; // pomocnicza zmienna wybranego koloru w masce (R lub G lub B)
 };
 
 #endif // __GUIMyFrame1__
